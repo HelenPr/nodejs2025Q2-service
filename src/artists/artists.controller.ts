78e73be794d37d1cc7538ff73a163eb3e67bae0b
LAPTOP-MPGEDP13\0fake <0fake0lake0@gmail.com>
2025-06-03 00:09:12 +0400

    feat: add artists module

---
+++ src/artists/artists.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Post, Put, Delete, Body, Param, HttpCode, HttpStatus } from '@nestjs/common';
+import { ArtistsService } from './artists.service';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+import { Artist } from './interfaces/artist.interface';
+
+@Controller('artist')
+export class ArtistsController {
+  constructor(private readonly artistsService: ArtistsService) {}
+
+  @Get()
+  getAllArtists(): Artist[] {
+    return this.artistsService.getAllArtists();
+  }
+
+  @Get(':id')
+  getArtistById(@Param('id') id: string): Artist {
+    return this.artistsService.getArtistById(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createArtist(@Body() createArtistDto: CreateArtistDto): Artist {
+    return this.artistsService.createArtist(createArtistDto);
+  }
+
+  @Put(':id')
+  updateArtist(
+    @Param('id') id: string,
+    @Body() updateArtistDto: UpdateArtistDto,
+  ): Artist {
+    return this.artistsService.updateArtist(id, updateArtistDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteArtist(@Param('id') id: string): void {
+    this.artistsService.deleteArtist(id);
+  }
+} 
---
+++ src/artists/artists.service.ts
@@ -0,0 +1,77 @@
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { Artist } from './interfaces/artist.interface';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+
+@Injectable()
+export class ArtistsService {
+  private artists: Artist[] = [];
+
+  private isValidUUID(id: string): boolean {
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidRegex.test(id);
+  }
+
+  getAllArtists(): Artist[] {
+    return this.artists;
+  }
+
+  getArtistById(id: string): Artist {
+    if (!this.isValidUUID(id)) {
+      throw new BadRequestException('Invalid artist ID format');
+    }
+
+    const artist = this.artists.find(artist => artist.id === id);
+    if (!artist) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    return artist;
+  }
+
+  createArtist(createArtistDto: CreateArtistDto): Artist {
+    const { name, grammy } = createArtistDto;
+    
+    const newArtist: Artist = {
+      id: randomUUID(),
+      name,
+      grammy,
+    };
+
+    this.artists.push(newArtist);
+    return newArtist;
+  }
+
+  updateArtist(id: string, updateArtistDto: UpdateArtistDto): Artist {
+    if (!this.isValidUUID(id)) {
+      throw new BadRequestException('Invalid artist ID format');
+    }
+
+    const artistIndex = this.artists.findIndex(artist => artist.id === id);
+    if (artistIndex === -1) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    const updatedArtist = {
+      ...this.artists[artistIndex],
+      ...updateArtistDto,
+    };
+
+    this.artists[artistIndex] = updatedArtist;
+    return updatedArtist;
+  }
+
+  deleteArtist(id: string): void {
+    if (!this.isValidUUID(id)) {
+      throw new BadRequestException('Invalid artist ID format');
+    }
+
+    const artistIndex = this.artists.findIndex(artist => artist.id === id);
+    if (artistIndex === -1) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    this.artists.splice(artistIndex, 1);
+  }
+} 
